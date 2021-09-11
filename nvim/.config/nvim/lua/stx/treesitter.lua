fd685a8337c305866bd0e38245ae318606ee1be4
Erik Simon <juststx@gmail.com>
2021-09-11 17:11:22 +0200

    prisma & golang

--- nvim/.config/nvim/lua/stx/treesitter.lua
+++ nvim/.config/nvim/lua/stx/treesitter.lua
@@ -9,4 +9,8 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  indentation = {
+    enable = true,
+    disable = { "prisma" }
+  }
 }
