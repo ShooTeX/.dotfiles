aa78cb57f07ddf052673d7be5281d9076da0752e
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-02-28 05:15:41 +0100

    if angular.json in root, disable tsserver rename capibility

--- nvim/.config/nvim/lua/stx/lsp.lua
+++ nvim/.config/nvim/lua/stx/lsp.lua
@@ -12,6 +12,12 @@ local on_attach = function(client, bufnr)
 		vim.api.nvim_buf_set_keymap(bufnr, ...)
 	end
 
+  if client.name == 'tsserver' then
+    if vim.fn.filereadable(client.config.root_dir .. '/angular.json') == 1 then
+      client.resolved_capabilities.rename = false
+    end
+  end
+
 	-- Mappings.
 	local opts = { noremap = true, silent = true }
 
