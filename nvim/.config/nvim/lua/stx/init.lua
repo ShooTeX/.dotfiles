e6185c3550c756a991da129bc8fb91d9704c9757
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-05-03 02:39:17 +0200

    : =!= .

--- nvim/.config/nvim/lua/stx/init.lua
+++ nvim/.config/nvim/lua/stx/init.lua
@@ -14,7 +14,7 @@ require("nvim-autopairs").setup({
 	fast_wrap = {},
 })
 
-require("cmp_tabnine.config").setup({
+require("cmp_tabnine.config"):setup({
 	max_lines = 1000,
 	max_num_results = 10,
 	sort = true,
