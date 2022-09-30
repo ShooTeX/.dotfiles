bf7ff9cae4dce5599984335ce9ae3ca355a2fb71
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-09-30 20:39:24 +0200

    trouble keymaps

--- nvchad/.config/nvim/lua/custom/mappings.lua
+++ nvchad/.config/nvim/lua/custom/mappings.lua
@@ -6,6 +6,17 @@ M.general = {
 	},
 }
 
+M.trouble = {
+	n = {
+		["<leader>xx"] = { "<cmd>TroubleToggle<cr>", "open trouble" },
+		["<leader>xw"] = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "open trouble workspace" },
+		["<leader>xd"] = { "<cmd>TroubleToggle document_diagnostics<cr>", "open trouble document" },
+		["<leader>xl"] = { "<cmd>TroubleToggle loclist<cr>", "open trouble loclist" },
+		["<leader>xq"] = { "<cmd>TroubleToggle quickfix<cr>", "open trouble quickfix" },
+		["gR"] = { "<cmd>TroubleToggle lsp_references<cr>", "open trouble lsp references" },
+	},
+}
+
 -- more keybinds!
 
 return M
