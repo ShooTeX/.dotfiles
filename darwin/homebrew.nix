d3893d70f1f5bace5ca1181ac1bf59f462119e58
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-03 20:32:08 +0200

    add gitify

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -7,6 +7,6 @@
       upgrade = true;
     };
     brews = [ "openssl@1.1" ];
-    casks = [ "whatsapp" "google-chrome" "tidal" "bitwarden" "discord" "parsec" "raycast" ];
+    casks = [ "whatsapp" "google-chrome" "tidal" "bitwarden" "discord" "parsec" "raycast" "gitify" ];
   };
 }
