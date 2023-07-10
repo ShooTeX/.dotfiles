a5373b8d8635c322c5c42f7fdddb7d3e398bccaf
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-07-10 09:23:38 +0200

    configurations update

--- configuration.nix
+++ configuration.nix
@@ -2,17 +2,37 @@
   nix.extraOptions =
     "	auto-optimise-store = true\n	experimental-features = nix-command flakes\n"
     + lib.optionalString (pkgs.system == "aarch64-darwin")
-    "	extra-platforms = x86_64-darwin aarch64-darwin\n";
+      "	extra-platforms = x86_64-darwin aarch64-darwin\n";
 
   programs.zsh.enable = true;
   services.nix-daemon.enable = true;
 
   fonts.fontDir.enable = true;
-  fonts.fonts = with pkgs; [ iosevka (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; }) ];
+  fonts.fonts = with pkgs; [
+    iosevka
+    (nerdfonts.override {
+      fonts = [ "NerdFontsSymbolsOnly" ];
+    })
+  ];
 
-  system.keyboard = {
-    enableKeyMapping = true;
-    remapCapsLockToEscape = true;
+  system = {
+    defaults = {
+      dock = {
+        autohide = true;
+        minimize-to-application = true;
+        show-recents = false;
+        static-only = true;
+        tilesize = 20;
+      };
+      finder = {
+        CreateDesktop = false;
+        QuitMenuItem = true;
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
   };
 
   security.pam.enableSudoTouchIdAuth = true;
