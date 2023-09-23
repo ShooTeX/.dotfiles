cdcf0d8a57c7af4b43615225be289785c82636d1
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-09-23 02:56:10 +0200

    add http4k

--- pkgs/http4k.nix
+++ pkgs/http4k.nix
@@ -5,33 +5,32 @@ with pkgs;
 let
   http4k = pkgs.stdenv.mkDerivation {
     name = "http4k";
-    src = fetchFromGitHub {
-      owner = "http4k";
-      repo = "toolbox-cli";
-      rev = "v5.0.0.0";
-      sha256 = "0000000000000000000000000000000000000000000000000000"; # Replace with the actual sha256
+    src = fetchurl {
+      url = "https://github.com/http4k/toolbox-cli/releases/download/v5.0.0.0/http4k-toolbox-5.0.0.0.zip"; # Replace with the URL to the ZIP file in the release assets.
+      sha256 = "VdK+NfwmIEw+aOtjGq5g3ZmA3x035FixqzT2SilS7xs="; # Replace with the actual SHA256 hash of the ZIP file.
     };
 
     nativeBuildInputs = [ pkgs.unzip ];
     buildInputs = [ pkgs.openjdk ];
 
+    unpackPhase = ''
+      mkdir -p $TMP/http4k-extracted
+      unzip -q $src -d $TMP/http4k-extracted
+      
+      mkdir -p $TMP/http4k
+      mv $TMP/http4k-extracted/*/* $TMP/http4k
+    '';
+
     installPhase = ''
-            # Remove .bat files
-            rm $out/bin/*.bat
+      mkdir -p $out/bin
+      # Remove .bat files
+      rm $TMP/http4k/bin/*.bat
 
-            # Make the main executable executable
-            chmod 0755 $out/bin/http4k
+      # Make the main executable executable
+      chmod 0755 $TMP/http4k/bin/http4k
 
-            # Install everything to the output path
-            cp -r $src/* $out/
-
-            # Create a wrapper script
-            mkdir -p $out/bin
-            cat > $out/bin/http4k <<EOF
-      #!/bin/sh
-      exec $out/bin/http4k-toolbox "\$@"
-      EOF
-            chmod +x $out/bin/http4k
+      # Install everything to the output path
+      cp -r $TMP/http4k/* $out
     '';
   };
 in
