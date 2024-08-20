353f33078a30c07a937f2a334b512f1f48d7bc3e
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-08-20 23:49:39 +0200

    use nix

--- home.nix
+++ home.nix
@@ -394,85 +394,91 @@
     recursive = true;
   };
 
-  xdg.configFile."aerospace/aerospace.toml" = {
-    enable = true;
-    text = ''
+  xdg.configFile."aerospace/aerospace.toml".source =
+    (pkgs.formats.toml { }).generate "config" {
       # Reference: https://github.com/i3/i3/blob/next/etc/config
-      start-at-login = true
+      start-at-login = true;
 
-      enable-normalization-flatten-containers = true
-      enable-normalization-opposite-orientation-for-nested-containers = true
+      enable-normalization-flatten-containers = true;
+      enable-normalization-opposite-orientation-for-nested-containers = true;
 
-      # Mouse follows focus when focused monitor changes
-      on-focused-monitor-changed = ['move-mouse monitor-lazy-center']
+      on-focus-changed = [ "move-mouse window-lazy-center" ];
 
-      [mode.main.binding]
-      alt-enter = ''''exec-and-forget osascript -e '
-      tell application "WezTerm"
-          do script
-          activate
-      end tell'
-      ''''
+      mode = {
+        main.binding = {
+          alt-enter = ''
+            exec-and-forget osascript -e '
+                  tell application "WezTerm"
+                      do script
+                      activate
+                  end tell'
+          '';
 
-      alt-h = 'focus left'
-      alt-j = 'focus down'
-      alt-k = 'focus up'
-      alt-l = 'focus right'
+          alt-h = "focus left";
+          alt-j = "focus down";
+          alt-k = "focus up";
+          alt-l = "focus right";
 
-      alt-shift-h = 'move left'
-      alt-shift-j = 'move down'
-      alt-shift-k = 'move up'
-      alt-shift-l = 'move right'
+          alt-shift-h = "move left";
+          alt-shift-j = "move down";
+          alt-shift-k = "move up";
+          alt-shift-l = "move right";
 
-      alt-f = 'fullscreen'
+          alt-f = "fullscreen";
 
-      alt-s = 'layout v_accordion' # 'layout stacking' in i3
-      alt-w = 'layout h_accordion' # 'layout tabbed' in i3
-      alt-e = 'layout tiles horizontal vertical' # 'layout toggle split' in i3
+          alt-s = "layout v_accordion"; # "layout stacking" in i3
+          alt-w = "layout h_accordion"; # "layout tabbed" in i3
+          alt-e =
+            "layout tiles horizontal vertical"; # "layout toggle split" in i3
 
-      alt-shift-space = 'layout floating tiling' # 'floating toggle' in i3
+          alt-shift-space = "layout floating tiling"; # 'floating toggle' in i3
 
-      alt-1 = 'workspace 1'
-      alt-2 = 'workspace 2'
-      alt-3 = 'workspace 3'
-      alt-4 = 'workspace 4'
-      alt-5 = 'workspace 5'
-      alt-6 = 'workspace 6'
-      alt-7 = 'workspace 7'
-      alt-8 = 'workspace 8'
-      alt-9 = 'workspace 9'
-      alt-0 = 'workspace 10'
+          alt-1 = "workspace 1";
+          alt-2 = "workspace 2";
+          alt-3 = "workspace 3";
+          alt-4 = "workspace 4";
+          alt-5 = "workspace 5";
+          alt-6 = "workspace 6";
+          alt-7 = "workspace 7";
+          alt-8 = "workspace 8";
+          alt-9 = "workspace 9";
+          alt-0 = "workspace 10";
 
-      alt-shift-1 = 'move-node-to-workspace 1'
-      alt-shift-2 = 'move-node-to-workspace 2'
-      alt-shift-3 = 'move-node-to-workspace 3'
-      alt-shift-4 = 'move-node-to-workspace 4'
-      alt-shift-5 = 'move-node-to-workspace 5'
-      alt-shift-6 = 'move-node-to-workspace 6'
-      alt-shift-7 = 'move-node-to-workspace 7'
-      alt-shift-8 = 'move-node-to-workspace 8'
-      alt-shift-9 = 'move-node-to-workspace 9'
-      alt-shift-0 = 'move-node-to-workspace 10'
+          alt-shift-1 = "move-node-to-workspace 1";
+          alt-shift-2 = "move-node-to-workspace 2";
+          alt-shift-3 = "move-node-to-workspace 3";
+          alt-shift-4 = "move-node-to-workspace 4";
+          alt-shift-5 = "move-node-to-workspace 5";
+          alt-shift-6 = "move-node-to-workspace 6";
+          alt-shift-7 = "move-node-to-workspace 7";
+          alt-shift-8 = "move-node-to-workspace 8";
+          alt-shift-9 = "move-node-to-workspace 9";
+          alt-shift-0 = "move-node-to-workspace 10";
 
-      alt-shift-i = 'resize smart -50'
-      alt-shift-o = 'resize smart +50'
+          alt-shift-i = "resize smart -50";
+          alt-shift-o = "resize smart +50";
 
-      on-focus-changed = ['move-mouse window-lazy-center']
+          alt-shift-semicolon = "mode service";
+        };
+        service.binding = {
+          esc = [ "reload-config" "mode main" ];
+          r = [ "flatten-workspace-tree" "mode main" ];
+          f = [ "layout floating tiling" "mode main" ];
+          backspace = [ "close-all-windows-but-current" "mode main" ];
+        };
+      };
 
-      alt-shift-semicolon = 'mode service'
-      [mode.service.binding]
-      esc = ['reload-config', 'mode main']
-      r = ['flatten-workspace-tree', 'mode main'] # reset layout
-      f = ['layout floating tiling', 'mode main'] # Toggle between floating and tiling layout
-      backspace = ['close-all-windows-but-current', 'mode main']
-
-      [gaps]
-      inner.horizontal = 10
-      inner.vertical =   10
-      outer.left =       10
-      outer.bottom =     10
-      outer.top =        10
-      outer.right =      10
-    '';
-  };
+      gaps = {
+        inner = {
+          horizontal = 10;
+          vertical = 10;
+        };
+        outer = {
+          left = 10;
+          bottom = 10;
+          top = 10;
+          right = 10;
+        };
+      };
+    };
 }
