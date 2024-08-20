b50a9c47c8bce09cae807992a979228fea00d894
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-08-20 23:18:40 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,9 @@ in {
     nix-daemon.enable = true;
     jankyborders = {
       enable = true;
-      active_color = "gradient(top_right=0x9992B3F5,bottom_left=0x9992B3F5)";
+      active_color = "gradient(top_right=0xfffc5c7d,bottom_left=0xff6a82fb)";
+      inactive_color = "0x00000000";
+      width = 10.;
     };
   };
 
