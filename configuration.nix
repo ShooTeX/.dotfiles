ad8edafe77d0212c4ab9acd7a8677ec830c0e230
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-11-14 17:53:36 +0100

    update

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@ let monaspace = pkgs.callPackage ./pkgs/fonts/monaspace.nix { };
 in {
   nix.optimise.automatic = true;
   nix.extraOptions =
-    "	auto-optimise-store = true\n	experimental-features = nix-command flakes\n"
+    "	auto-optimise-store = false\n	experimental-features = nix-command flakes\n"
     + lib.optionalString (pkgs.system == "aarch64-darwin")
     "	extra-platforms = x86_64-darwin aarch64-darwin\n";
 
