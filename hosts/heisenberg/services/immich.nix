6f0e5a418676f4c1a6337d63e7230752ce7d03b4
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-01-18 16:40:05 +0100

    setup immich backups

--- hosts/heisenberg/services/immich.nix
+++ hosts/heisenberg/services/immich.nix
@@ -1,13 +1,28 @@
 { config, ... }:
 {
-  services.immich = {
-    enable = true;
+  services = {
+    immich = {
+      enable = true;
+    };
+
+    borgmatic.settings = {
+      source_directories = [
+        config.services.immich.mediaLocation
+      ];
+      postgresql_databases = [
+        {
+          inherit (config.services.immich.database) name;
+          username = config.services.immich.database.user;
+        }
+      ];
+    };
+
+    caddy.virtualHosts."immich.dottex.world" = {
+      useACMEHost = "dottex.world";
+      extraConfig = ''
+        reverse_proxy localhost:${toString config.services.immich.port}
+      '';
+    };
   };
 
-  services.caddy.virtualHosts."immich.dottex.world" = {
-    useACMEHost = "dottex.world";
-    extraConfig = ''
-      reverse_proxy localhost:${toString config.services.immich.port}
-    '';
-  };
 }
