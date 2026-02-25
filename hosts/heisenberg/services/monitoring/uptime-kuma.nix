7b829c82504f8c8e0ce4e28c1903b0f1c3850071
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-02-25 22:34:16 +0100

    backup uptime kuma

--- hosts/heisenberg/services/monitoring/uptime-kuma.nix
+++ hosts/heisenberg/services/monitoring/uptime-kuma.nix
@@ -4,14 +4,20 @@
     uptime-kuma = {
       enable = true;
       settings = {
-        UPTIME_KUMA_PORT = "4251";
+        PORT = "4251";
       };
+    };
+
+    borgmatic.settings = {
+      source_directories = [
+        config.services.settings.DATA_DIR
+      ];
     };
 
     caddy.virtualHosts."uptime-kuma.dottex.world" = {
       useACMEHost = "dottex.world";
       extraConfig = ''
-        reverse_proxy localhost:${toString config.services.uptime-kuma.settings.UPTIME_KUMA_PORT}
+        reverse_proxy localhost:${toString config.services.uptime-kuma.settings.PORT}
       '';
     };
   };
