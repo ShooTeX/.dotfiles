97bba35c0025c7c2dc4f4f34010991b28aa300eb
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-03-01 18:30:52 +0100

    backup adgaurd config

--- hosts/heisenberg/services/adguardhome.nix
+++ hosts/heisenberg/services/adguardhome.nix
@@ -1,18 +1,26 @@
 { config, ... }:
 {
-  services.adguardhome = {
-    enable = true;
+  services = {
+    adguardhome = {
+      enable = true;
 
-    settings = { };
-    port = 3001;
-  };
+      settings = { };
+      port = 3001;
+    };
 
-  services.caddy.virtualHosts = {
-    "adguard.dottex.world" = {
-      useACMEHost = "dottex.world";
-      extraConfig = ''
-        reverse_proxy localhost:${toString config.services.adguardhome.port}
-      '';
+    caddy.virtualHosts = {
+      "adguard.dottex.world" = {
+        useACMEHost = "dottex.world";
+        extraConfig = ''
+          reverse_proxy localhost:${toString config.services.adguardhome.port}
+        '';
+      };
+    };
+
+    borgmatic.settings = {
+      source_directories = [
+        "/var/lib/AdGuardHome/AdGuardHome.yaml"
+      ];
     };
   };
 
