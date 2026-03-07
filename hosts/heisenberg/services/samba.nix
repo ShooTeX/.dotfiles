e2fde8fcc3d23d294b90de8e615a6101b4027646
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-03-08 00:51:26 +0100

    samba config

--- hosts/heisenberg/services/samba.nix
+++ hosts/heisenberg/services/samba.nix
@@ -2,42 +2,70 @@ let
   shares_dir = "/mnt/storage/shares";
 in
 {
-  services.samba = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      global = {
-        "netbios name" = "DOTTEX-WORLD";
-        "workgroup" = "WORKGROUP";
-        "server string" = "heisenberg";
-        "server role" = "standalone server";
-        "log level" = "1";
-      };
-      stx = {
-        path = "${shares_dir}/stx";
-        browseable = "yes";
-        writeable = "yes";
-        "valid users" = "stx";
-      };
-      punkt = {
-        path = "${shares_dir}/punkt";
-        browseable = "yes";
-        writeable = "yes";
-        "valid users" = "punkt";
-      };
-      shared = {
-        path = "${shares_dir}/shared";
-        browseable = "yes";
-        writeable = "yes";
-        "valid users" = "stx punkt";
-      };
-      public = {
-        path = "${shares_dir}/public";
-        browseable = "yes";
-        writeable = "no";
-        "guest ok" = "yes";
+  services = {
+    samba = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        global = {
+          "netbios name" = "DOTTEX-WORLD";
+          "server string" = "DOTTEX WORLD";
+          "workgroup" = "WORKGROUP";
+          "server role" = "standalone server";
+          "client min protocol" = "SMB2";
+          "server min protocol" = "SMB2";
+          "fruit:metadata" = "stream";
+          "fruit:model" = "MacSamba";
+          "fruit:posix_rename" = "yes";
+          "fruit:veto_appledouble" = "no";
+          "fruit:wipe_intentionally_left_blank_rfork" = "yes";
+          "fruit:delete_empty_adfiles" = "yes";
+        };
+        stx = {
+          path = "${shares_dir}/stx";
+          browseable = "yes";
+          writeable = "yes";
+          "valid users" = "stx";
+          "vfs objects" = "catia fruit streams_xattr";
+        };
+        punkt = {
+          path = "${shares_dir}/punkt";
+          browseable = "yes";
+          writeable = "yes";
+          "valid users" = "punkt";
+        };
+        shared = {
+          path = "${shares_dir}/shared";
+          browseable = "yes";
+          writeable = "yes";
+          "valid users" = "stx punkt";
+          "vfs objects" = "catia fruit streams_xattr";
+        };
+        public = {
+          path = "${shares_dir}/public";
+          browseable = "yes";
+          writeable = "no";
+          "guest ok" = "yes";
+        };
       };
     };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        workstation = true;
+      };
+    };
+
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    restic.backups.storagebox.paths = [ shares_dir ];
   };
 
   systemd.tmpfiles.rules = [
@@ -48,5 +76,4 @@ in
     "d ${shares_dir}/public          0755 root  root   -"
   ];
 
-  services.restic.backups.storagebox.paths = [ shares_dir ];
 }
