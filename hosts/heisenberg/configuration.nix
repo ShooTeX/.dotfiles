01c0ef8a4abc0536085072dd352db4aab3cf949e
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-03-08 00:03:07 +0100

    enable autoScrub

--- hosts/heisenberg/configuration.nix
+++ hosts/heisenberg/configuration.nix
@@ -66,4 +66,9 @@
       defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
     };
   };
+
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [ "/mnt/storage" ];
+  };
 }
