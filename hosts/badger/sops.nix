f0c3fd9f64532206f07f885310c0d9153be5c3cd
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-03-14 16:07:36 +0100

    fix sops

--- hosts/badger/sops.nix
+++ hosts/badger/sops.nix
@@ -1,5 +1,26 @@
+{ lib, pkgs }:
 {
+  environment.etc."sops-key.txt" = {
+    text = ''
+      AGE-PLUGIN-YUBIKEY-18TMGYQVZSD0W33SXG6T7T
+      AGE-PLUGIN-YUBIKEY-1MLP8SQVZSU9SEWSTDSSM4
+    '';
+  };
+  environment.systemPackages = [
+    pkgs.age-plugin-yubikey
+    pkgs.age
+    pkgs.sops
+  ];
+
   sops = {
+    age = {
+      keyFile = "/etc/sops-key.txt";
+      generateKey = false;
+    };
+
+    environment = {
+      PATH = "${lib.makeBinPath [ pkgs.age-plugin-yubikey ]}:$PATH";
+    };
     defaultSopsFile = ./secrets/main.enc.yaml;
   };
 }
