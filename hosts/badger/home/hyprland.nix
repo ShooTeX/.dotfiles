c52535fdedaeb52c6e135fd189bcc9b9f75fa905
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-03-17 20:53:52 +0100

    window stuff

--- hosts/badger/home/hyprland.nix
+++ hosts/badger/home/hyprland.nix
@@ -8,6 +8,9 @@
     enable = true;
 
     settings = {
+      "$mod" = "SUPER";
+      "$terminal" = "ghostty";
+
       env = [
         "HYPRCURSOR_THEME,Bibata-Modern-Classic"
         "HYPRCURSOR_SIZE,24"
@@ -18,15 +21,22 @@
       general = {
         "col.active_border" = "rgba(76946Aff)";
         "col.inactive_border" = "rgba(2D4F67ff)";
-        border_size = 2;
+        border_size = 1;
       };
       input = {
         accel_profile = "flat";
         sensitivity = 0;
       };
-      monitor = [ "DP-2,2560x1440@360,0x0,1" ];
-      "$mod" = "SUPER";
-      "$terminal" = "ghostty";
+      monitor = [
+        "DP-1,2560x1440@60,0x0,1"
+        "DP-2,2560x1440@360,2560x0,1,bitdepth,10,cm,auto"
+      ];
+      workspace = [
+        "1,monitor:DP-2,default:true"
+        "r[2-5],monitor:DP-2"
+        "6,monitor:DP-1,default:true"
+        "r[7-0],monitor:DP-1"
+      ];
       bind = [
         "$mod, Return, exec, $terminal"
         "$mod, Space, exec, walker"
@@ -44,6 +54,11 @@
         "$mod SHIFT, K, swapwindow, u"
         "$mod SHIFT, L, swapwindow, r"
 
+        "$mod CTRL, H, movewindow, l"
+        "$mod CTRL, J, movewindow, d"
+        "$mod CTRL, K, movewindow, u"
+        "$mod CTRL, L, movewindow, r"
+
         "$mod SHIFT, F, fullscreen"
         "$mod, F, fullscreen, 1"
 
@@ -52,12 +67,22 @@
         "$mod, 3, workspace, 3"
         "$mod, 4, workspace, 4"
         "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 0"
 
         "$mod SHIFT, 1, movetoworkspace, 1"
         "$mod SHIFT, 2, movetoworkspace, 2"
         "$mod SHIFT, 3, movetoworkspace, 3"
         "$mod SHIFT, 4, movetoworkspace, 4"
         "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 0"
       ];
       bindl = [
         ", XF86AudioPlay, exec, playerctl play-pause"
