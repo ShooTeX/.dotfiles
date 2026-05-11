915929ee98abd93e43ddabeda18f6ea75258bea9
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-05-11 14:17:13 +0200

    update

--- hosts/heisenberg/services/web-servers/caddy.nix
+++ hosts/heisenberg/services/web-servers/caddy.nix
@@ -37,7 +37,7 @@
         "github.com/caddy-dns/cloudflare@v0.2.2"
         "github.com/ueffel/caddy-brotli@v1.6.0"
       ];
-      hash = "sha256-9dVmVsqlzWz5nhe3thdYJBrsL855cP+yjgZ67Bxv16o=";
+      hash = "sha256-Efji1i+D/BihufXcfeiyrVbcjkV3U4YJz8yQ92/c0W8=";
     };
   };
 
